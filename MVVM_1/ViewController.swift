1165a6a7f1efd07cb95ef6600c1315441676ab1f
Ruslan <ruslanfilistovich@gmail.com>
2019-11-07 00:13:17 +0300

    Initial Commit

---
+++ MVVM_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_1
+//
+//  Created by Ruslan on 11/7/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
