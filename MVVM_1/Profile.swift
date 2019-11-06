5291b22c15f9329182adb1b22546acf82939f139
Ruslan <ruslanfilistovich@gmail.com>
2019-11-07 00:30:38 +0300

    MVC Project

---
+++ MVVM_1/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  MVVM_1
+//
+//  Created by Ruslan on 11/7/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
--- MVVM_1/ViewController.swift
+++ MVVM_1/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var secondNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    var profile: Profile? {
+        didSet {
+            guard let profile = profile else { return }
+            self.nameLabel.text = profile.name
+            self.secondNameLabel.text = profile.secondName
+            self.ageLabel.text = "\(profile.age)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        profile = Profile(name: "Steve", secondName: "Jobs", age: 56)
     }
 
 
+    
 }
 
